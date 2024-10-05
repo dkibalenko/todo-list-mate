4265a9aef209b20b8dd7f8380f4ca62111edf9d1
Denys Kibalenko <dennieking87@gmail.com>
2024-10-05 12:11:08 +0200

    Feat: add task create form and view

--- todo_list_app/urls.py
+++ todo_list_app/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path("create/", views.TaskCreteView.as_view(), name="add-task"),
 ]
 
 app_name = "todo_list_app"
--- todo_list_app/views.py
+++ todo_list_app/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 from django.views import generic
 
 from .models import Task, Tag
+from .forms import TaskForm
 
 class IndexView(generic.ListView):
     template_name = "todo_list_app/index.html"
     model = Task
     context_object_name = "todo_list"
     queryset = Task.objects.order_by("done", "-datetime").prefetch_related("tags")
+
+
+class TaskCreteView(generic.CreateView):
+    template_name = "todo_list_app/task_form.html"
+    model = Task
+    form_class = TaskForm
+    success_url = "/"
