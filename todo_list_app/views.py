362b520d7ddec8f89ed93c90e03b8b6809c0275e
Denys Kibalenko <dennieking87@gmail.com>
2024-10-05 11:50:41 +0200

    Feat: add index view

--- todo_list_app/views.py
+++ todo_list_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.views import generic
 
+from .models import Task, Tag
+
 class IndexView(generic.ListView):
-    ...
+    template_name = "todo_list_app/index.html"
+    model = Task
+    context_object_name = "todo_list"
+    queryset = Task.objects.order_by("done", "-datetime").prefetch_related("tags")
